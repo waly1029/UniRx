da912f857f7e2a65aae06d021f64dc2c51a050ba
student <student@MacBook-Pro-03.local>
2016-12-17 15:42:09 +0900

    12/25 https://tcagame2016.slack.com/messages/tca_programmer/

---
+++ UniRxTest/Assets/Scripts/UinRx.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+using UnityEngine.UI;
+
+public class UinRx : MonoBehaviour {
+	Subject<int>subject = new Subject<int>();
+	public Button button;
+
+	void Start(){
+		button.onClick.AsObservable( ).Take( 40 )
+			.Select( x => 1 )
+			.Scan( 0, (element, acc ) => element + acc )
+			.Where( x => ( x % 3 == 0 || x % 5 == 0 ) )
+			.Subscribe( x => Debug.Log( x + " + hoge" ) );
+		
+		button.onClick.AsObservable( ).Take( 40 )
+			.Select( x => 1 )
+			.Scan( 0, (element, acc ) => element + acc )
+			.Where( x => ( x % 3 != 0 && x % 5 != 0 ) )
+			.Subscribe( x => Debug.Log( x ) );
+	}
+		
+}
